c4d7ea1aee837bd509bc94a67f605388336d1901
huangwenke <2438652648@qq.com>
2023-02-07 23:42:37 +0800

    api-passenger项目骨架搭建

---
+++ api-passenger/src/main/java/com/wowo/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,15 @@
+package com.wowo.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wowo
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
---
+++ api-passenger/src/main/java/com/wowo/apipassenger/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.wowo.apipassenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wowo
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test api passenger";
+    }
+}
